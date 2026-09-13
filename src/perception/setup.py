45a29018eddc01343784ff72d7e929f0b0d27508
Ankan Mondal <ankanmondal07@yahoo.com>
2026-09-13 14:34:40 +0530

    oakD_perception

---
+++ src/perception/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'perception'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dronzer',
+    maintainer_email='ankanmondal07@yahoo.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'tracker_node = perception.oakd_tracker_node:main'
+        ],
+    },
+)
